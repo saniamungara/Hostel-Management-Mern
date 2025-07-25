e21a330c56dde76bc61ef2e4dc289f105f5216f0
Sania Mungara <saniamungara@gmail.com>
2025-07-25 10:48:37 +0530

    fix: use env variable for MongoDB URI

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/employee', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("✅ Connected to MongoDB"))
